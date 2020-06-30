41ea2ebd3989e59751107347b1c2be1846221c10
DipsyKerchon <65933362+DipsyKerchon@users.noreply.github.com>
2020-06-30 19:50:12 +0300

    почему ошибка?

---
+++ test.py
@@ -0,0 +1,30 @@
+class Num1:
+    def __init__(self, a):
+        self.a = a
+
+    def draw(self):
+        print(f'Num1 {self.a}')
+
+class Num2:
+    def __init__(self, b):
+        self.b = b
+
+    def drw(self):
+        print(f'Num2 {self.b}')
+
+class Num3(Num1, Num2):
+    def __init__(self, a, b):
+        Num1.__init__(self, a)
+        Num2.__init__(self, b)
+
+    def draw(self):
+        print(f'Pencil {self.a}, {self.b}')
+
+s = Num1(1)
+s.draw()
+
+pan = Num2(2)
+pan.drw()
+
+p = Num3()
+p.draw()
