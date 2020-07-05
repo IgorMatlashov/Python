67e698e0ee368f83465b126c144aa77ef420426d
DipsyKerchon <65933362+DipsyKerchon@users.noreply.github.com>
2020-07-05 13:27:58 +0300

    Update task_7.py

--- task_7.py
+++ task_7.py
@@ -4,21 +4,27 @@
 # Проверьте корректность полученного результата.
 
 class ComplexNumber:
-    def __init__(self, a):
+    def __init__(self, a, b, z = 'j'):
         self.a = a
+        self.b = b
+        self.z = z
 
     def __add__(self, other):
-        return f'Sum: {self.a + other.a}'
+        r1 = self.a + other.a
+        r2 = self.b + other.b
+        return f'Sum: {r1} {"+" if r2 > 0 else "-"} {abs(r2)} * {self.z}'
 
     def __mul__(self, other):
-        return f'Mul: {self.a * other.a}'
+        r1 = (self.a * other.a) - (self.b * other.b)
+        r2 = (self.b * other.a) + (self.a * other.b)
+        return f'Mul: {r1} {"+" if r2 > 0 else "-"} {abs(r2)} * {self.z}'
 
     def __str__(self):
         return f'result operation:'
 
 
-z_1 = ComplexNumber(2+2j)
-z_2 = ComplexNumber(3+2j)
+z_1 = ComplexNumber(2, 6)
+z_2 = ComplexNumber(2, -12)
 print(z_1)
 print(z_1 + z_2)
 print(z_1 * z_2)
